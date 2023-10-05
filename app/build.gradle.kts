adf1b27568d3b792a651844172904d3f6b0344bf
Acocalypso <acocalypso@gmail.com>
2023-10-05 16:15:03 +0200

    remove singing instruction

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,14 +32,14 @@ android {
         } else "null")
     }
 
-    signingConfigs {
+    /*signingConfigs {
         create("release") {
             storeFile = file(project.property("KEYSTORE_FILE").toString())
             storePassword = project.property("KEYSTORE_PASSWORD").toString()
             keyAlias = project.property("SIGNING_KEY_ALIAS").toString()
             keyPassword = project.property("SIGNING_KEY_PASSWORD").toString()
         }
-    }
+    }*/
 
     buildTypes {
         debug {
@@ -51,7 +51,7 @@ android {
             if (!pluginManager.hasPlugin("com.google.gms.google-services")) {
                 the<CrashlyticsExtension>().mappingFileUploadEnabled = false
             }
-            signingConfig = signingConfigs.getByName(name)
+            // signingConfig = signingConfigs.getByName(name)
         }
     }
     buildFeatures.buildConfig = true
